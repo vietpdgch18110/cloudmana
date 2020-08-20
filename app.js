a0d5f76b0a0516eae6ae629e220264e2cafe0e78
giangnkt2k <thangiangss2@gmail.com>
2020-08-21 00:21:40 +0700

    ok

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const engines = require("consolidate");
 const { body } = require("express-validator");
 const app = express();
 var fs = require("fs");
-
+var mongodb =require('mongodb')
 var bodyParser = require("body-parser");
 const { finished } = require("stream");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,16 +17,16 @@ app.engine("hbs", engines.handlebars);
 app.set("views", "./views");
 app.set("view engine", "hbs");
 
-// ket noi database
 var MongoClient = require("mongodb").MongoClient;
 var url =
-  "mongodb+srv://buiquanghuy742:buiquanghuy742@cluster0.2sybn.mongodb.net/test";
+  "mongodb+srv://phamducviet:phamducviet16@cluster0.bipjf.mongodb.net/test";
 
 //localhost:5000
-app.get("/product", async function (req, res) {
+app.get("/", async function (req, res) {
   let client = await MongoClient.connect(url);
-  let dbo = client.db("Product");
-  let result = await dbo.collection("product").find({}).toArray(); // print product in product table
+  let dbo = client.db("Staff");
+  let result = await dbo.collection("staff").find({}).toArray(); // print product in product table
+  
   res.render("index", { model: result });
 });
 
@@ -35,20 +35,11 @@ app.get("/remove", async (req, res) => {
   let id = req.query.id;
   var ObjectID = require("mongodb").ObjectID;
   let client = await MongoClient.connect(url);
-  let dbo = client.db("Product");
-  await dbo.collection("product").deleteOne({ _id: ObjectID(id) });
-  res.redirect("/product");
+  let dbo = client.db("Staff");
+  await dbo.collection("staff").deleteOne({ _id: ObjectID(id) });
+  res.redirect("/");
 });
 
-// DELETE USERS
-app.get("/removeusers", async (req, res) => {
-  let id = req.query.id;
-  var ObjectID = require("mongodb").ObjectID;
-  let client = await MongoClient.connect(url);
-  let dbo = client.db("Product");
-  await dbo.collection("user").deleteOne({ _id: ObjectID(id) });
-  res.redirect("/users");
-});
 
 
 // INSERT
@@ -57,38 +48,28 @@ app.get("/insert", (req, res) => {
 });
 app.post("/doInsert", async (req, res) => {
   let inputName = req.body.txtName;
-  let inputQuantity = req.body.txtQuantity;
-  let inputPrice = req.body.txtPrice;
-  let inputDescription = req.body.txtDescription;
-
-  let newProduct = {
-    name: inputName,
-    quantity: inputQuantity,
-    price: inputPrice,
-    description: inputDescription,
-  };
+  let inputAge = req.body.txtAge;
   
-  if (isNaN(inputName.substr(0,2))){
-    let errorModel = {nameError: "2 first characters must be numbers"};
-    res.render("insert", {model:errorModel}) 
-  }else{
-    let client = await MongoClient.connect(url);
-    let dbo = client.db("Product");
-    await dbo.collection("product").insertOne(newProduct);
-    res.redirect("/product");
-  }
+  let inputGender = req.body.txtGender;
+  let inputNumber =req.body.txtNumber;
 
-  if (isNaN(inputPrice)){
-    let errorModel = {priceError: "Price must be a number"};
+  let newStaff = {
+    name: inputName,
+    age: inputAge,
+    gender: inputGender,
+    number: inputNumber,
+  };
+
+  if (isNaN(inputAge)){
+    let errorModel = {priceError: "Age must be a number"};
     res.render("insert", {model:errorModel}) 
   } else{
     
-  }
+  } 
   let client = await MongoClient.connect(url);
-  let dbo = client.db("Product");
-  await dbo.collection("product").insertOne(newProduct);
-  res.redirect("/product");
- 
+    let dbo = client.db("Staff");
+    await dbo.collection("staff").insertOne(newStaff);
+    res.redirect("/");
 });
 
 // SEARCH PRODUCT FUNCTION
@@ -97,66 +78,43 @@ app.get("/doSearch", async (req, res) => {
   // let name_search_U = req.query.txtSearchU;
   // connect to database Mongodb
   let client = await MongoClient.connect(url);
-  let dbo = client.db("Product");
+  let dbo = client.db("Staff");
   let result = await dbo
-    .collection("product")
+    .collection("staff")
     .find({ name: new RegExp(name_search, "i") })
     .toArray();
   res.render("index", { model: result });
 });
 
-// REGISTER
-app.get("/register", function (req, res) {
-  res.render("register");
-});
-
-var fileName = "users.txt";
-// connect to hbs
-app.post("/doRegister", async (req, res) => {
-  let inputName = req.body.txtName;
-  let inputEmail = req.body.txtEmail;
-
-  // // check validation
-  if (inputName.length < 3 ) {
-    let errorModel = {
-      // emailError: "Email must have more than 3 characters",
-      nameError: "Invalid",
-    };
-    res.redirect("/register");
-  } 
-  else {
-    let data = inputName + ";" + inputEmail + "\n";
-    fs.appendFile(fileName, data, function (err) {
-      res.redirect("/users");     
-    });
-
+app.get("/update", async (req,res)=>{
+    let id=req.query.id;
+    var ObjectID = mongodb.ObjectID;
     let client = await MongoClient.connect(url);
-    let dbo = client.db("Product");
-    await dbo
-    .collection("user")
-    .insertOne({ name: inputName, email: inputEmail });
-    res.redirect("/users");
-  }
-
-  
-});
-
-// get all users
-app.get("/users", async (req, res) => {
-  //     let result = [];
-  //     fs.readFile(fileName,'utf8',function(error,data){
-  //         let allUsers = data.split("\n");
-  //         for(i=0;i<allUsers.length;i++){
-  //             if(allUsers[i].trim().length !=0){
-  //                 let nameX = allUsers[i].split(';')[0];
-  //                 let emailX = allUsers[i].split(';')[1];
-  //                 result.push({name:nameX,email:emailX})
-  //             }
-  //         }
+    let dbo = client.db('Staff');
+    let user =await dbo.collection("staff").find({_id:ObjectID(id.toString())}).toArray();
+    console.log(user);
+   res.render('update',{model:user});
+})
+app.post("/doUpdate",async(req,res)=>{
+  let id =req.body.txtid;
+  let inputName = req.body.txtName;
+  let inputAge = req.body.txtAge;
+  let inputGender = req.body.txtGender;
+  let inputNumber =req.body.txtNumber;
+  var ObjectID = mongodb.ObjectID;
   let client = await MongoClient.connect(url);
-  let dbo = client.db("Product");
-  let result = await dbo.collection("user").find({}).toArray();
-  res.render("users", { model: result });
-});
+  let dbo = client.db('Staff');
+ // let user =await dbo.collection("staff").find({_id:ObjectID(id.toString())}).toArray();
+  let t= await dbo.collection("staff").
+    updateOne({ "_id":ObjectID(id.toString())},
+                  {$set:{name:inputName.toString(),
+                       age:inputAge.toString(),
+                     gender:inputGender.toString(),
+                     number:inputNumber.toString()
+                    }})
+    console.log(t);    
+
+    res.redirect('/');
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
